dc818897f737bce631acc05182e6d80fc39b37d6
Benjamin Fejzagic <benjamin.fejzagic@hotmail.com>
2020-11-24 11:09:09 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show]
+  resources :bookings, only: [:new, :create, :show]
+  resources :reviews, only: [:new, :create]
+  resources :dashboard, only: [:index, :show]
 end
