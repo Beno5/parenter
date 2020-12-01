5e4801252cd55bdc5d240567d36ac1f219d89741
Dominique Zimmermann <dzim@gmx.net>
2020-12-01 12:36:59 +0100

    calendar structure for parent fixed

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,8 @@
 class DashboardsController < ApplicationController
   def index
-   
+    @parent = current_user
     @bookings = Booking.all
+    @schedule = helpers.events_hash(Booking.where(parent: current_user), :parent)
   end
 
-  
 end
