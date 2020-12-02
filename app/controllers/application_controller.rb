d13def3ebae73b68afca9129b4e5d1daa3b00692
Benjamin Fejzagic <benjamin.fejzagic@hotmail.com>
2020-12-02 11:36:27 +0100

    edit done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :bio, :photo, :price, :max_cap, :number_of_kids, :can_host, :is_nanny, :year_of_birth])
+  end
+
 end
