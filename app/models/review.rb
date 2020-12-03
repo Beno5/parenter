a5c92e9117d29abe809c767c7278b2177d076699
Benjamin Fejzagic <benjamin.fejzagic@hotmail.com>
2020-12-03 14:33:54 +0100

    d

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :nanny, class_name: "User", foreign_key: "nanny_id", primary_key: :id
   belongs_to :parent, class_name: "User", foreign_key: "parent_id", primary_key: :id
-  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5,
-  message: '%{value} is not in category' }
+ 
 end
