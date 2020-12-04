a065c26ae45b8f237744cf4e492d3df332f8a606
Benjamin Fejzagic <benjamin.fejzagic@hotmail.com>
2020-12-04 08:54:45 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -359,5 +359,16 @@ puts 'Creating fake users...'
   review4 = Review.create(nanny: mario, parent: hans, content: 'Mario is very cheap but also worth every penny!', rating: 5)
   review6 = Review.create(nanny: martina, parent: philip, content: 'I love Martina. She is very flexible and always comes on time', rating: 4)
   review7 = Review.create(nanny: martina, parent: arthur, content: 'I love the fact that Martina is able to over to our place as sometimes I do not have time to drop of my daughter at her place', rating: 5)
-puts 'Finished!'
+  review5 = Review.create(nanny: alice, parent: greg, content: 'Alice was very good and kids love her!', rating: 5)
+  review5 = Review.create(nanny: helen, parent: greg, content: 'Helen was okej!', rating: 3)
+  review5 = Review.create(nanny: lucy, parent: greg, content: 'Lucy was not there on time!', rating: 1)
+  review5 = Review.create(nanny: teresa, parent: greg, content: 'Sarah was very good and kids love her!', rating: 4)
+  review5 = Review.create(nanny: bob, parent: greg, content: 'Bob was not so frendly!', rating: 3)
+  review5 = Review.create(nanny: alfred, parent: greg, content: 'Alfred was okey!', rating: 4)
+  review5 = Review.create(nanny: kasey, parent: greg, content: 'Kesey was very rude in one moment and I do not recommend her!', rating: 1)
+  review5 = Review.create(nanny: darlene, parent: greg, content: 'Darlene was bad!', rating: 2)
+  review5 = Review.create(nanny: catherine, parent: greg, content: 'Catherine was very good and kids love her!', rating: 5)
+  review5 = Review.create(nanny: hallie, parent: greg, content: 'Hallie was cold to the kids!', rating: 3)
+
+    puts 'Finished!'
 
