0dbfa22dcd3b897ae677115e7238fef1148c2361
Benjamin Fejzagic <benjamin.fejzagic@hotmail.com>
2020-12-04 09:34:23 +0100

    boookings

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,11 @@ class DashboardsController < ApplicationController
   def index
     @review = Review.new
     @parent = current_user
-    @bookings = Booking.all
+    if current_user.is_nanny == false
+      @bookings = Booking.where(parent: current_user)
+    elsif current_user.is_nanny == true
+      @bookings = Booking.where(nanny: current_user)
+    end
     @schedule = helpers.events_hash(Booking.where(parent: current_user), :parent)
   end
 end
